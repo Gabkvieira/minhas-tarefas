0c587c38f8ed121bd5474fbc4a91cfc0c54febd3
Gabriel Vieira <gabkv99@hotmail.com>
2024-12-16 10:52:20 -0300

    adicionando pesquisa e filtro de tarefas

---
+++ src/store/reducers/filtro.ts
@@ -0,0 +1,31 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import * as enums from '../../utils/enums/Tarefa'
+
+type FiltrosState = {
+  termo?: string
+  criterio: 'prioridade' | 'status' | 'todas'
+  valor?: enums.Prioridade | enums.Status
+}
+
+const initialState: FiltrosState = {
+  termo: '',
+  criterio: 'todas'
+}
+
+const filtroSlice = createSlice({
+  name: 'filtro',
+  initialState,
+  reducers: {
+    alterarTermo: (state, action: PayloadAction<string>) => {
+      state.termo = action.payload
+    },
+    alterarFiltro: (state, action: PayloadAction<FiltrosState>) => {
+      state.criterio = action.payload.criterio
+      state.valor = action.payload.valor
+    }
+  }
+})
+
+export const { alterarTermo, alterarFiltro } = filtroSlice.actions
+
+export default filtroSlice.reducer
